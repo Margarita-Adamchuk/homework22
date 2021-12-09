73dd3af7269c0105fe86afca977d0828a93947c0
Margarita Adamchuk <margarita.adamchuk1988@gmail.com>
2021-12-09 14:15:21 +0300

    fill in the table with visitors

--- DZ6.py
+++ DZ6.py
@@ -1,38 +1,32 @@
 # Создать класс Medical_Center
+import sqlite3
 
 class Medical_Center:
     counter = 0
-    database_name = []
-    database_age = []
-    database_doktor = []
+    db1 = sqlite3.connect("Visitors")
+    courser = db1.cursor()
+
+    courser.execute('''CREATE TABLE IF NOT EXISTS table1
+                            (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            Name TEXT,
+                            Age INT,
+                            Doctor TEXT)''')
+
+    db1.commit()
 
     def __init__(self, name):
         self.name = name
-        print(f"{self.name} дабро пожалавать в наш медецинский центр!")
         self.age = int(input("Сколько вам лет: "))
-        self.doktor = input("К какому врачу вы идете: ")
-        self.namber = 0
+        self.doctor = input("К какому врачу вы идете: ")
         Medical_Center.counter += 1
-        Medical_Center.database_name.append(self.name)
-        Medical_Center.database_age.append(self.age)
-        Medical_Center.database_doktor.append(self.doktor)
+        Medical_Center.courser.execute('''INSERT INTO table1(Name, Age, Doctor)
+                                      VALUES (?, ?, ?)''', (self.name, self.age, self.doctor))
+        Medical_Center.db1.commit()
 
-    def age_determination_max(self):
-        print("Возраст самого старшего поселителя:")
-        return max(Medical_Center.database_age)
 
-    def age_determination_min(self):
-        print("Возраст самого молодого поселителя:")
-        return min(Medical_Center.database_age)
+if __name__ == '__main__':
+    print("Дабро пожаловать в наш медецинский центр!")
+    visitors1 = Medical_Center(input("Каквас зовут"))
+    Medical_Center.courser.execute('''SELECT * FROM table1''')
 
-    def __str__(self):
-        print("Информация о всех поситителях")
-        for item in range(len(Medical_Center.database_name) - 1):
-            print("_____________________________________")
-            print(f"| {item} | {Medical_Center.database_name[self.namber]} |"
-                  f" {Medical_Center.database_age[self.namber]} | "
-                  f"{Medical_Center.database_doktor[self.namber]}")
-            print("______________________________________")
-            self.namber =+ 1
-
-a = list()
+    print(Medical_Center.courser.fetchall())
